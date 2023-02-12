b988d64804534f65f8a15a902ac8dd74eba03e12
Mateus Gomes Lucas <mateusglucas@hotmail.com>
2023-02-12 02:13:48 -0300

    Fix typo

--- src/config_priv_key.py
+++ src/config_priv_key.py
@@ -3,6 +3,6 @@ from pwinput import pwinput
 
 print('##### AXS Auto-staker configuration #####')
 print('')
-print('ONLY PASTE YOUR PRIVATE KEY BELOW IF YOU KNOW WHAT YOU ARE DOING AND UNDERSTANDS ALL THE RISKS INVOLVED!')
+print('ONLY PASTE YOUR PRIVATE KEY BELOW IF YOU KNOW WHAT YOU ARE DOING AND UNDERSTAND ALL THE RISKS INVOLVED!')
 print('')
 keyring.set_password('ronin','priv_key', pwinput(prompt='Ronin wallet private key: ', mask='*'))
